efa1b4c1e472c26416a438f60ec3cade09a2a682
Manikandan-prog <84707867+Manikandan-prog@users.noreply.github.com>
2021-10-19 18:29:00 +0530

    Update App.java

--- src/main/java/aaaa/App.java
+++ src/main/java/aaaa/App.java
@@ -6,13 +6,6 @@ package aaaa;
  */
 public class App 
 {
-
-// 	private void aaa() {
-// 		if("xx".length() > 7) {
-			
-// 		}
-// 	}
-
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
